ab9f40b7cdc83cd02a4d0c8db71bd7e333a4307b
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-07-30 00:38:56 +0600

    mixed op

---
+++ mixedop.js
@@ -0,0 +1,23 @@
+var first = "mobarak";
+var second = "tobarak";
+
+var fullName = first + second;
+console.log(fullName);
+// output mobaraktobarak
+
+console.log(40 + "2");
+// output 402
+console.log("40" + 2);
+// output 402
+console.log(40 - "20");
+// output 20
+console.log("50" - 10);
+// output 40
+console.log(40 * "20");
+// output 800
+console.log("50" * 10);
+// output 500
+console.log("50" / 10);
+// output 5
+console.log(50 / "10");
+// output 5
