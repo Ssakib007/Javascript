477d1334e5587f182432041491805f86ee03bef0
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-07-29 14:54:31 +0600

    variables

---
+++ math.js
@@ -0,0 +1,15 @@
+// math needed for programming. 
+// 1 number : positive, negative
+// 2. Integer : odd, even
+// 3. float ; 5.6
+// 4. Operation: +, -, *, /
+// 5. More operations: %, []
+// 6. Percentage: 
+// 7. Sum
+// 8. Average
+// 9. Max/Min
+// 10. Series
+// 11. Area : Triangle
+// 12. 
+
+
---
+++ variable.js
@@ -0,0 +1,12 @@
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+
+var age;
+console.log(money);
+console.log(price);
+
+var age = 35;
+age = 44;
+console.log(age)
