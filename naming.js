cfe48eda5feacad27cd822e621fac5023f5958d5
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-07-29 15:52:57 +0600

    naming of variables

---
+++ naming.js
@@ -0,0 +1,36 @@
+//   no keywords in variable name
+var false = 45;
+var var = 50; 
+
+// no space or gap in variable name
+var is happy = false;
+var ishappy = true;
+
+// no quote
+var "address" = "kochu khet";
+var address = "kochu khet";
+
+
+// can not start with a number. but number later is allowed
+var money = 45;
+var money2= 55;
+var 3money = 88;
+
+
+// name is case sensitive
+address
+address
+ADDRESS
+addRess
+
+// how to write a long variable name
+
+var my current home address = " andor killa bandar bon";
+var mycurrenthomeaddress = " andor killa bandar bon";
+var my_current_home_address = " andor killa bandar bon";  // snake case
+var myCurrentHomeAddress = " andor killa bandar bon"; // camel case
+var MyCurrentHomeAddress = " andor killa bandar bon"; // pascal case
+
+var name = "john";
+VAR name = "john";
+Var name = "john";
