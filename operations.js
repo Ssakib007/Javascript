8c43aaed0379b1790e9d0cf6359d0a2bd4dcfbf5
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-07-30 00:26:45 +0600

    mathematical operations

---
+++ operations.js
@@ -0,0 +1,53 @@
+// Addition
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+// output: 30
+
+// Subtraction
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice - eggPrice;
+console.log(totalPrice);
+// output: 10
+
+// Multiplication
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice * eggPrice;
+console.log(totalPrice);
+// output: 200
+
+// division
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice / eggPrice;
+console.log(totalPrice);
+// output: 2
+
+// Modulus
+var num1 = 5;
+var num2 = 2;
+var result = num1 % num2;
+console.log(result);
+// output: 1
+
+// shorthands
+var price = 35;
+// price = price + 10;
+price += 10;
+console.log(price);
+// output 45
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000;
+console.log(fridge);
+// output 34000
+
+var orangePrice = 30;
+// orangePrice = orangePrice * 3;
+orangePrice *= 3;
+console.log(orangePrice);
+// output 270
